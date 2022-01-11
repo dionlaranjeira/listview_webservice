62a0fe40a2ce6f71da9dc5baf62ab3ce530c09fd
Dion Laranjeira <dionribeiro.rr@gmail.com>
2022-01-11 11:09:45 -0400

    Listando itens locais

--- lib/Home.dart
+++ lib/Home.dart
@@ -8,8 +8,39 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+
+  List _itens = [];
+
+  void _carregarItens(){
+    for(int i=0; i<=10; i++){
+      Map<String, dynamic> item = Map();
+      item["titulo"] = "Título ${i} Lorem ipsum dolor sit met.";
+      item["descricao"] = "Descrição ${i} Lorem ipsum dolor sit met.";
+      _itens.add(item);
+    }
+  }
+
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    _carregarItens();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Lista e WebServices"),
+      ),
+      body: Container(
+        padding: const EdgeInsets.all(24),
+        child: ListView.builder(
+            itemCount: _itens.length,
+            itemBuilder: (context, indice){
+
+              return ListTile(
+                title: Text(_itens[indice]["titulo"].toString()),
+                subtitle: Text(_itens[indice]["descricao"].toString()),
+              );
+            },
+            ),
+      ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:listview_webservice/Home.dart';
 
 void main() {
-  runApp(const MaterialApp(
+  runApp(MaterialApp(
     home: Home(),
   ));
 }
