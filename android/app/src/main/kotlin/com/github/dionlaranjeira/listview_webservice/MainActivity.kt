561373406d500435e057c6230efa5da296e03ac1
Dion Laranjeira <dionribeiro.rr@gmail.com>
2022-01-11 10:38:42 -0400

    Initial commit

---
+++ android/app/src/main/kotlin/com/github/dionlaranjeira/listview_webservice/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.github.dionlaranjeira.listview_webservice
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:listview_webservice/Home.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    home: Home(),
+  ));
+}
